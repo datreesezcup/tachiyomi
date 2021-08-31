b0aa532cbc3b36ea23524e6f4e18502566f424d1
Hunter Nickel <dementor9@yahoo.com>
2021-08-31 09:28:28 -0600

    Fix stub source using another source's icon, hide selection menu when adapter is empty

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/database/DatabaseSourceItem.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/database/DatabaseSourceItem.kt
@@ -10,6 +10,7 @@ import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.databinding.DatabaseSourceItemBinding
 import eu.kanade.tachiyomi.source.LocalSource
 import eu.kanade.tachiyomi.source.Source
+import eu.kanade.tachiyomi.source.SourceManager
 import eu.kanade.tachiyomi.source.icon
 import eu.kanade.tachiyomi.util.system.LocaleHelper
 
@@ -44,15 +45,17 @@ class DatabaseSourceItem(val source: Source, val mangaCount: Int) :
         private val binding = DatabaseSourceItemBinding.bind(view)
 
         fun bind(item: DatabaseSourceItem) {
-            binding.title.text = if (item.source.id != LocalSource.ID) {
+            binding.title.text = if (item.source.id != LocalSource.ID && item.source !is SourceManager.StubSource) {
                 "${item.source.name} (${LocaleHelper.getSourceDisplayName(item.source.lang, itemView.context)})"
             } else item.source.name
 
             binding.description.text = itemView.context.getString(R.string.database_source_manga_count, item.mangaCount)
             binding.checkbox.isChecked = adapter.isSelected(bindingAdapterPosition)
+
             itemView.post {
                 when {
                     item.source.id == LocalSource.ID -> binding.thumbnail.setImageResource(R.mipmap.ic_local_source)
+                    item.source is SourceManager.StubSource -> binding.thumbnail.setImageDrawable(null)
                     item.source.icon() != null -> binding.thumbnail.setImageDrawable(item.source.icon())
                 }
             }
